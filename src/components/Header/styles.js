298efb719dcd17f99c50a0f0ab352515a989ca92
Mellyssa Monteiro <monteirodbm@gmail.com>
2019-12-20 17:35:17 -0300

    atualizando teste

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,15 +1,58 @@
 import styled from 'styled-components';
 
-export const LogopraDiferenciar = styled.h4`
-   img {
+export const LogopraDiferenciar = styled.a`
 
+   img {
+      margin-left: 40px;
+      position: absolute;
+      top: 30px;
+      width: 80px;
    }
+
 @media (max-width: 980px){
+
   img {
+    margin-left: 15px;
     width: 70px;
     position: absolute;
-    top: 30px;
+    top: 18px;
   }
 }
+`
+
+export const Container = styled.nav`
+
+    display: flex;
+    justify-content: flex-end;
+    flex-flow: row nowrap;
+    align-items: center;
+    position: relative;
+    padding: 25px;
+    top: 25px;
+    margin-right: 40px;
+
+@media (max-width:980px){
+    flex-flow: column nowrap;
+    position: absolute;
+    right: 0;
+    margin-right: 10px;
+    top: 85px;
+    width: 250px;
+    background-color: gray;
+  } 
+
+  a {
+    text-decoration: none;
+    color: #000;
+    font-family: Arial, Helvetica, sans-serif;
+    padding: 15px;
+    font-weight: bold;
+    font-size: 15px;
+    
+  }
+
+  a:hover{
+    color: rgba(220,220,220);
+  }
 
 `
