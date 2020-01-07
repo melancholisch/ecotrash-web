ea734b95255067c5f5e59b066721287aae1f076c
Mellyssa Monteiro <monteirodbm@gmail.com>
2020-01-07 17:52:16 -0300

    adicionando banners e iniciando a página quem somos

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-// import {Title} from './styles';
 // import Routes from '../../routes';
 
 import Header from '../../components/Header/Header';
+import {ContainerTexto} from './styles';
 import Footer from '../../components/Footer/Footer';
 
 
@@ -10,9 +10,18 @@ export default function About() {
   return (
     <>
     <Header />
+    <ContainerTexto>
+    <h1>PROPÓSITO</h1>
+    <p>
+    Da era digital emerge uma nova lógica que rege a forma como nos comunicamos, aprendemos, interagimos e nos 
+    potencializamos. Os jovens dessa era podem desafiar o sistema vigente e, conectados, programar novas soluções 
+    para os problemas do mundo. Impulsionar esse movimento é o nosso propósito.Por meio do empoderamento digital, 
+    queremos ampliar o horizonte de oportunidades para os jovens em situação de vulnerabilidade social. 
+    Alguns indicadores reforçam a urgência de nossa atuação:</p>
+    </ContainerTexto>
     <Footer />
     
-    <h1>TESTE</h1>
+    
     </>
   );
-}
+  }
---
+++ src/pages/About/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ContainerTexto = styled.div`
+    display: block;
+    justify-content: center;
+    align-items: center;
+    h1, p{
+    width: 500px;
+    text-align: center;
+    }
+`
