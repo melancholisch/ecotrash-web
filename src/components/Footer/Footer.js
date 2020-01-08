56ff1308bbe7efea21d5194f11b5d8fd37882306
Mellyssa Monteiro <monteirodbm@gmail.com>
2020-01-08 12:58:02 -0300

    fotos deletadas e footer com redirecionamentos feitos

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,20 +7,16 @@ import { Container, Todos} from './styles';
 const footer = props =>(
     
         <Container>
-            
-            
             <div>
             <a href='https://www.facebook.com/EcoTrash-102756437926770/' >Facebook</a>
             <a href='https://instagram.com/ecotrashrio' >Instagram</a>
             <a href='https://'>ecotrashrio@gmail.com</a>
             <a href='https://'>+55 21 99936-6562 </a>    
             </div>
-            
             <Todos>
                 <p>© 2019 por ecotrash <br />
                 Todos os direitos reservados</p>
-            </Todos>  
-                
+            </Todos>      
         </Container>
 );
 
