ab297e3e7ffda180b836c417a668aab6652f3e97
Mellyssa Monteiro <monteirodbm@gmail.com>
2020-02-07 18:08:55 -0300

    mapa e botão funcionando

--- src/pages/Points/Geo/stylesGeoloc.js
+++ src/pages/Points/Geo/stylesGeoloc.js
@@ -1,16 +1,27 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  width: 80vw;
-  margin: 0 auto;
-
-
-  @media (max-width:980px){
+    #map { 
+    height: 480px; 
+    width: 90%; 
+    margin: 0 auto; 
+    
+    @media (max-width:980px){
     max-width: 90vw;
+    z-index:0;
+    }
 
   }
+    button{
+      display: flex;
+      margin: 0 auto;
+      padding: 20px;
+      background-color: rgb(133, 184, 27);
+      color: #fff;
+      border-radius: 15px;
+      font-weight: bold;
+  }
+    button:hover{
+      color: rgb(220, 220, 220);
+    }
 `;
--- src/pages/Points/index.js
+++ src/pages/Points/index.js
@@ -11,7 +11,7 @@ export default function Points() {
   return (
     <>
     <Header />
-    <Geoloc />
+    <Geoloc/>
     <Footer />
     </>
   );
