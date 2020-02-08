8b9946df8c4b8872addf2085576ef40570cb2f6d
Mellyssa Monteiro <monteirodbm@gmail.com>
2020-02-08 06:51:35 -0300

    configurando mais localizaçoes ao mapa

--- src/pages/Points/Geo/Geoloc.js
+++ src/pages/Points/Geo/Geoloc.js
@@ -9,7 +9,7 @@ export default class Map extends Component {
       this.state = {
           lng: -22.904732,
           lat: -43.176408,
-          zoom: 13,
+          zoom: 12,
       };
   };
 
@@ -20,8 +20,17 @@ export default class Map extends Component {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
 
+  
+    // SABOR SAUDE CENTRO
     L.marker([-22.904732, -43.176408]).addTo(this.map)
         .bindPopup('<b style="color: green">Descarte seus equipamentos aqui!</b><br />Sabor Saúde<br/> Rua da Quitanda nº32, Centro - Rio de janeiro</p>');
+    // MUNDO VERDE BOTAFOGO
+    L.marker([-22.949193, -43.187831]).addTo(this.map)
+        .bindPopup('<b style="color: green">Descarte seus equipamentos aqui!</b><br />Mundo verde <br/> Rua São Clemente nº164, Botafogo - Rio de janeiro</p>');
+    // MUNDO VERDE ZN
+    L.marker([-22.884931, -43.284891]).addTo(this.map)
+        .bindPopup('<b style="color: green">Descarte seus equipamentos aqui!</b><br />Mundo verde <br/> Av Dom Helder Câmara nº5474, Piedade - Rio de janeiro</p>');
+  
 
   };
 
--- src/pages/Points/Geo/stylesGeoloc.js
+++ src/pages/Points/Geo/stylesGeoloc.js
@@ -9,17 +9,22 @@ export const Container = styled.div`
     @media (max-width:980px){
     max-width: 90vw;
     z-index:0;
+
     }
   }
+
     button{
       display: flex;
       margin: 0 auto;
+      position: relative;
+      bottom: 15px;
       padding: 20px;
       background-color: rgb(133, 184, 27);
       color: #fff;
       border-radius: 15px;
       font-weight: bold;
   }
+
     button:hover{
       color: rgb(220, 220, 220);
     }
