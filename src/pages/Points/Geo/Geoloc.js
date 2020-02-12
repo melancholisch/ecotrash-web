ffe6f7d0565fc19f68c8d34aa2227616fcebc424
Mellyssa Monteiro <monteirodbm@gmail.com>
2020-02-12 18:19:08 -0300

    modificações nos markers da pagina pontos de coleta e mudança de mapa. shortcut icon adicionado

--- src/pages/Points/Geo/Geoloc.js
+++ src/pages/Points/Geo/Geoloc.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import L from 'leaflet'
 //import { Icon } from 'leaflet';
+//import markerPontos '../../../assets/marker-ecotrash-teste1.png';
+//import markerUsuario '../../../assets/marker-ecotrash-sombra.png';
 import { Container } from './stylesGeoloc';
 
 export default class Map extends Component {
@@ -9,34 +11,59 @@ export default class Map extends Component {
       this.state = {
           lng: -22.904732,
           lat: -43.176408,
-          zoom: 12,
+          zoom: 12.5,
       };
   };
 
   componentDidMount(){
     this.map = L.map('map').setView([this.state.lng, this.state.lat], this.state.zoom);
 
-    L.tileLayer('https://c.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
 
-  
+        // ICONE PERSONALIZADO 
+        let pontoColeta = L.icon({
+          iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-green.png',
+          shadowUrl: 'http://leafletjs.com/examples/custom-icons/leaf-shadow.png',
+    
+          iconSize: [38, 95],
+          shadowSize: [50, 64],
+          
+          popupAnchor: [-3, -76]
+          });
+
+
     // SABOR SAUDE CENTRO
-    L.marker([-22.904732, -43.176408]).addTo(this.map)
+    L.marker([-22.904732, -43.176408], {icon:pontoColeta}).addTo(this.map)
         .bindPopup('<b style="color: green">Descarte seus equipamentos aqui!</b><br />Sabor Saúde<br/> Rua da Quitanda nº32, Centro - Rio de janeiro</p>');
     // MUNDO VERDE BOTAFOGO
-    L.marker([-22.949193, -43.187831]).addTo(this.map)
+    L.marker([-22.949193, -43.187831], {icon:pontoColeta}).addTo(this.map)
         .bindPopup('<b style="color: green">Descarte seus equipamentos aqui!</b><br />Mundo verde <br/> Rua São Clemente nº164, Botafogo - Rio de janeiro</p>');
     // MUNDO VERDE ZN
-    L.marker([-22.884931, -43.284891]).addTo(this.map)
+    L.marker([-22.884931, -43.284891], {icon:pontoColeta}).addTo(this.map)
         .bindPopup('<b style="color: green">Descarte seus equipamentos aqui!</b><br />Mundo verde <br/> Av Dom Helder Câmara nº5474, Piedade - Rio de janeiro</p>');
   
 
   };
 
-  // POSIÇÃO DO USUARIO
-
     displayLocation = () => {
+
+      // ICONE USUARIO 
+      // TROCAR QUANDO DESCOBRIR COMO COLOCAR IMAGENS DIRETAMENTE DO COMPUTADOR
+      /*let pontoUsuario = L.icon({
+        iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-red.png',
+        shadowUrl: 'http://leafletjs.com/examples/custom-icons/leaf-shadow.png',
+  
+        iconSize: [38, 95],
+        shadowSize: [50, 64],
+        
+        popupAnchor: [-3, -76]
+        });
+      */
+
+      // PEGANDO A POSICAO DO USUARIO
+
       navigator.geolocation.getCurrentPosition((position) => {
         let longi = position.coords.longitude;
         let lati = position.coords.latitude;
